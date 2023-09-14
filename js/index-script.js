85dd28dc20cb61f2daaa373fafa858986ef84a1e
danilosheen <c.danilo.f.silva@gmail.com>
2023-09-14 17:16:32 -0300

    feat: add icon, content and responsitivy

--- js/index-script.js
+++ js/index-script.js
@@ -6,3 +6,38 @@ document.addEventListener("DOMContentLoaded", function () {
       menuNav.classList.toggle("active");
     });
   });
+
+//video modal
+const openVideoBtn = document.getElementById("openVideo");
+const videoModal = document.getElementById("videoModal");
+const videoFrame = document.getElementById("videoFrame");
+
+openVideoBtn.addEventListener("click", () => {
+  abrirVideo();
+});
+
+function abrirVideo() {
+  videoFrame.style.display = "block";
+  videoModal.style.display = "block";
+
+  // Adicionar event listener para fechar o modal clicando fora dele
+  document.addEventListener("click", fecharAoClicarFora);
+}
+
+function fecharVideo() {
+  videoFrame.style.display = "none";
+  videoModal.style.display = "none";
+  
+  // Pausar o vídeo (se necessário)
+  videoFrame.src = videoFrame.src;
+  
+  // Remover event listener para fechar o modal clicando fora dele
+  document.removeEventListener("click", fecharAoClicarFora);
+}
+
+function fecharAoClicarFora(event) {
+  if (event.target === videoModal) {
+    fecharVideo();
+  }
+}
+
