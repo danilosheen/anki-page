bf087ae81e3b977f892598b26f39c53a2d5b05f7
danilosheen <c.danilo.f.silva@gmail.com>
2023-09-14 19:48:38 -0300

    fix: responsivity

--- js/index-script.js
+++ js/index-script.js
@@ -1,3 +1,5 @@
+// função para exibir o menu hamburguer
+
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const menuNav = document.querySelector(".container-nav-mobile");
@@ -6,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
       menuNav.classList.toggle("active");
     });
   });
+
+
 
 //video modal
 const openVideoBtn = document.getElementById("openVideo");
@@ -41,3 +45,20 @@ function fecharAoClicarFora(event) {
   }
 }
 
+
+
+
+// Função para rolar suavemente até a seção desejada
+function rolarParaDiv(id) {
+  var elemento = document.getElementById(id);
+  elemento.scrollIntoView({ behavior: "smooth" });
+}
+
+// Event listener para links de ancoragem
+document.addEventListener("click", function (event) {
+  if (event.target.tagName === "A" && event.target.getAttribute("href").startsWith("#")) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    var id = event.target.getAttribute("href").substring(1); // Obtém o ID da âncora
+    rolarParaDiv(id); // Chama a função para rolar suavemente até a seção
+  }
+});
