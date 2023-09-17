9be24f164a87b0de91892b02988d3f06b8e21cd4
danilosheen <c.danilo.f.silva@gmail.com>
2023-09-17 12:19:42 -0300

    fix: flow js

--- js/index-script.js
+++ js/index-script.js
@@ -10,6 +10,24 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 //------------------------------------------------
+
+// Função para rolar suavemente até a seção desejada
+function rolarParaDiv(id) {
+  var elemento = document.getElementById(id);
+  elemento.scrollIntoView({ behavior: "smooth" });
+}
+
+// Event listener para links de ancoragem
+document.addEventListener("click", function (event) {
+  if (event.target.tagName === "A" && event.target.getAttribute("href").startsWith("#")) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    var id = event.target.getAttribute("href").substring(1); // Obtém o ID da âncora
+    rolarParaDiv(id); // Chama a função para rolar suavemente até a seção
+  }
+});
+
+//-----------------------------------------------------
+
 //video modal
 const openVideoBtn = document.getElementById("openVideo");
 const videoModal = document.getElementById("videoModal");
@@ -44,21 +62,6 @@ function fecharAoClicarFora(event) {
   }
 }
 //------------------------------------------------
-
-// Função para rolar suavemente até a seção desejada
-function rolarParaDiv(id) {
-  var elemento = document.getElementById(id);
-  elemento.scrollIntoView({ behavior: "smooth" });
-}
-
-// Event listener para links de ancoragem
-document.addEventListener("click", function (event) {
-  if (event.target.tagName === "A" && event.target.getAttribute("href").startsWith("#")) {
-    event.preventDefault(); // Impede o comportamento padrão do link
-    var id = event.target.getAttribute("href").substring(1); // Obtém o ID da âncora
-    rolarParaDiv(id); // Chama a função para rolar suavemente até a seção
-  }
-});
 
 
 // faq
