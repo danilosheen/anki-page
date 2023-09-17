df201b446af13d935985e1f39d2951611bd9d0dc
danilosheen <c.danilo.f.silva@gmail.com>
2023-09-17 15:19:09 -0300

    refact: clean code smooth

--- js/start.js
+++ js/start.js
@@ -1,18 +1,5 @@
-// // Função para rolar suavemente até a seção desejada
-// function rolarParaDiv(id) {
-//   var elemento = document.getElementById(id);
-//   elemento.scrollIntoView({ behavior: "smooth" });
-// }
 
-// // Event listener para links de ancoragem
-// document.addEventListener("click", function (event) {
-//   if (event.target.tagName === "A" && event.target.getAttribute("href").startsWith("#")) {
-//     event.preventDefault(); // Impede o comportamento padrão do link
-//     var id = event.target.getAttribute("href").substring(1); // Obtém o ID da âncora
-//     rolarParaDiv(id); // Chama a função para rolar suavemente até a seção
-//   }
-// });
-
+// função para carregar smooth
 document.addEventListener("DOMContentLoaded", function () {
   // Verifique se o JavaScript está disponível
   if (typeof window.addEventListener === 'function') {
