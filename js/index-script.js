911032bc36fc1776db1efa850a8c6407e497c258
danilosheen <c.danilo.f.silva@gmail.com>
2023-09-28 17:16:58 -0300

    feat: add link desconto

--- js/index-script.js
+++ js/index-script.js
@@ -48,6 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Lista de chaves e seus links correspondentes
 const listaDeChaves = {
+  "indique": "https://pay.kiwify.com.br/PYRmcpA",
   "cronograma": "https://pay.kiwify.com.br/TbUfka9",
   "trechos25": "https://pay.kiwify.com.br/PYRmcpA?afid=b4AsGXQo",
   "umtrecho": "https://pay.kiwify.com.br/PYRmcpA?afid=i4tuwJxd",
